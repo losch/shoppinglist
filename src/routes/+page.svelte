7b52a15dac7540ea6534e0633e9affc7a7a2340d
Ilari Nummila <ilarinum@gmail.com>
2023-08-27 17:35:58 +0300

    Update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,26 +5,12 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>Ostoslista</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
+	<h1>Ostoslista</h1>
 	<Counter />
 </section>
 
